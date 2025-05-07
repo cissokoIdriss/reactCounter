6f00339a8a651bfbaa8675725a34f84e00b3515e
cissokoIdriss <cissokoi@merrimack.edu>
2025-05-06 20:22:34 -0400

    added state for counter and buttons for increase, decrease and reset

--- App.js
+++ App.js
@@ -2,10 +2,19 @@ import { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to my simple counter</Text>
+      <Text>counter {counter}</Text>
+      <Button title="+" onPress={() => setCounter(counter + 1)} />
+      <Button
+        title="-"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Button title="reset" onPress={() => setCounter(0)} />
     </View>
   );
 }
